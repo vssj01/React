777061fa307a5988cc4c223aca0b920407287e92
Sri Sai Jyothi <vssj01@gmail.com>
2024-06-27 17:05:18 +0530

    3 -Laying the Foundation

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,22 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-      "div", 
-      {id: "parent"},
-      React.createElement(
-            "div" , 
-            {id: "child"},
-            [
-                  React.createElement("h1", {}, "I'm h1 tag"),
-                  React.createElement("h2", {}, "I'm h2 tag")
-            ]
-            
-      )
-)
 
-console.log(parent);
+// JSX (transpiled before it reached the JS) - PARCEL - Babel
+
+//JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
+);
+
+// React Fragment - behaves like an empty tag
+const HeadingComponent = () => (
+  <>
+    <div id="container">
+      {Title()}
+      <Title />
+      <Title></Title>
+      <h1 className="heaidng">Namaste React Fucntional Component</h1>
+    </div>
+    <div id="container-2"></div>
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
