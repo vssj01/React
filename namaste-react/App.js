4005b3b23afcde4cfb99713a432a8cc4e3d52395
Sri Sai Jyothi <vssj01@gmail.com>
2024-06-27 11:35:03 +0530

    Ep 02 Igniting out App

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,19 +1,5 @@
-
-// const heading = React.createElement("h1", {id: "heading", xyz: 'abc'}, "Hello World from React");
-
-// console.log(heading); // React Object, not html element
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// ---------- Let us render nested html elements -----------------
-/**
-<div id="parent">
-      <div id="child">
-            <h1></h1>
-      </div>
-</div> 
-ReactElement(Object) => HTML(Browser understands)
- **/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
       "div", 
---
+++ namaste-react/README.md
@@ -0,0 +1,20 @@
+Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR- Hot Module Replacement
+- File Watching Algorithm - wriiten in C++
+- Caching -Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differencial Bundling- support older browsers
+- Diagnostics - good errors on UI
+- Error Handling
+- HTTPs
+- Tree shaking - removes unused code
+- Different dev and prod bundles
